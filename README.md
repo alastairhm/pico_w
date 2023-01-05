dca02c3277334c050bde2bd55b3b8f6ddeabdb33
Alastair Montgomery <alastair.montgomery@bjss.com>
2023-01-05 15:10:49 +0000

    tweak to wifi test

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Raspberry Pi Pico W
 
 Some example for Raspberry Pi Pico W with MicroPython
+
+* [blink.py](blink.py) basic on board LED blink
+* [wifi.py](wifi.py) testing WIFI connection, reads connections details from `wifi.txt` stored on the Pico format  single line "SSID PASSWORD".
+
--- wifi.py
+++ wifi.py
@@ -1,27 +1,31 @@
 import time
 import network
 
-ssid = 'Wireless Network'
-password = 'The Password'
+f = open("wifi.txt", "r")
+wifi_details = f.readlines()
+f.close()
+
+ssid = wifi_details[0].split(" ")[0]
+password = wifi_details[0].split(" ")[1]
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(ssid, password)
 
 # Wait for connect or fail
+print("Connection to", ssid)
 max_wait = 10
 while max_wait > 0:
- if wlan.status() < 0 or wlan.status() >= 3:
- break
- max_wait -= 1
- print('waiting for connection...')
- time.sleep(1)
+    if wlan.status() < 0 or wlan.status() >= 3:
+        break
+    max_wait -= 1
+    print("waiting for connection...")
+    time.sleep(1)
 
 # Handle connection error
 if wlan.status() != 3:
- raise RuntimeError('network connection failed')
+    raise RuntimeError("network connection failed")
 else:
- print('connected')
- status = wlan.ifconfig()
- print( 'ip = ' + status[0] )
- 
+    print("connected")
+    status = wlan.ifconfig()
+    print("ip = " + status[0])
