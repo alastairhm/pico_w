c0081c3e0514d8815025b76b5eeced6991793a61
Alastair Montgomery <alastair.montgomery@bjss.com>
2023-01-06 09:18:18 +0000

    Internal temp sensor

---
+++ internal_temp.py
@@ -0,0 +1,22 @@
+import machine
+import utime
+
+class GetInternalTemp:
+    """Read the internal temp sensor"""
+
+    def __init__(self):
+        """Init"""
+        self.sensor_temp = machine.ADC(4)
+        self.convert = 3.3 / 65535
+        self.temperature = 0
+
+    def get_temp(self):
+        """Read the Internal Temp Sensor"""
+        reading = self.sensor_temp.read_u16() * convert
+        self.temperature = 27 - (reading - 0.706)/0.001721
+
+if __name__ == "__main__":
+    temp = GetInternalTemp()
+    while True:
+        print(temp.get_temp())
+        utime.sleep(1)
