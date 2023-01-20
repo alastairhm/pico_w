7b4669365babf54e9cbfb30ddf19d1fcdb0821d7
Alastair Montgomery <alastair.montgomery@bjss.com>
2023-01-20 13:55:08 +0000

    Testing format update

--- led_matrix.py
+++ led_matrix.py
@@ -1,34 +1,16 @@
 import max7219
 import time
 from machine import Pin, SPI
+from framebuf import MONO_HLSB, MONO_HMSB, MONO_VLSB
 
-spi = SPI(0, baudrate=10000000, polarity=1, phase=0, sck=Pin(2), mosi=Pin(3))
-ss = Pin(5, Pin.OUT)
-display = max7219.Matrix8x8(spi, ss, 1)
-
-display.brightness(1)
-display.fill(0)
-
-for x in range(0, 8):
-    for y in range(0, 8):
-        display.pixel(x, y, 1)
+def display_str(display, text):
+    for c in range(0, len(text)):
+        display.text(text[c], 0, 0, 1)
         display.show()
-        time.sleep(0.025)
-
-for x in range(0, 8):
-    for y in range(0, 8):
-        display.pixel(x, y, 0)
+        time.sleep(0.3)
+        display.text(text[c], 0, 0, 0)
         display.show()
-        time.sleep(0.025)
-
-text = "Hello World!"
-for c in range(0, len(text)):
-    display.text(text[c], 0, 0, 1)
-    display.show()
-    time.sleep(0.3)
-    display.text(text[c], 0, 0, 0)
-    display.show()
-    time.sleep(0.05)
+        time.sleep(0.05)
 
 GLYPHS = {
     "X": [
@@ -72,6 +54,32 @@ GLYPHS = {
         [0, 0, 0, 0, 0, 0, 0, 0],
     ],
 }
+
+spi = SPI(0, baudrate=10000000, polarity=1, phase=0, sck=Pin(2), mosi=Pin(3))
+ss = Pin(5, Pin.OUT)
+display = max7219.Matrix8x8(spi, ss, 1)
+
+display.brightness(1)
+display.fill(0)
+
+# Test all pixels light up
+for x in range(0, 8):
+    for y in range(0, 8):
+        display.pixel(x, y, 1)
+        display.show()
+        time.sleep(0.01)
+
+for x in range(0, 8):
+    for y in range(0, 8):
+        display.pixel(x, y, 0)
+        display.show()
+        time.sleep(0.01)
+
+#Test Text
+text = "Hello World!"
+display_str(display, text)
+
+#Test Custom Glyphs
 display.text_from_glyph("X", GLYPHS)
 display.show()
 display.fill(0)
@@ -85,14 +93,19 @@ for i in range(0, len(animation)):
     time.sleep(0.25)
 
 
-array = [
-        [1, 0, 0, 0, 0, 0, 0, 0],
-        [1, 1, 1, 0, 0, 1, 1, 0],
-        [1, 1, 1, 0, 0, 1, 1, 0],
-        [1, 0, 0, 0, 0, 0, 0, 0],
-        [1, 0, 1, 1, 1, 1, 0, 0],
-        [1, 1, 0, 0, 0, 0, 1, 0],
-        [1, 0, 0, 0, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0, 0, 0, 0],
-]
- 
+#Test Different Mapping Options
+display = max7219.Matrix8x8(spi, ss, 1, MONO_VLSB)
+display.fill(0)
+display.text("P")
+display.show()
+time.sleep(0.5)
+display = max7219.Matrix8x8(spi, ss, 1, MONO_HMSB)
+display.fill(0)
+display.text("P")
+display.show()
+time.sleep(0.5)
+display = max7219.Matrix8x8(spi, ss, 1, MONO_HLSB)
+display.fill(0)
+display.text("P")
+display.show()
+time.sleep(0.5)
