3ecd6268c36cc5474828cce24c4a9da0e952993d
Alastair Montgomery <alastair.montgomery@bjss.com>
2023-01-27 14:45:22 +0000

    Added rotate option

--- numbers.py
+++ numbers.py
@@ -4,8 +4,15 @@ from machine import Pin, SPI
 from framebuf import MONO_HLSB, MONO_HMSB, MONO_VLSB
 
 
-def compact_number(display, number, offx=0, offy=0):
-    """Display compact numbers so two digits fit in 8x5 LED grid"""
+def compact_number(display, number, offx=0, offy=0, rotate=0):
+    """
+    Display compact numbers so two digits fit in 8x5 LED grid
+    display = instance of max7219 display
+    number  = number to display
+    offx    = x offest position
+    offy    = y offest position
+    rotate  = number of times to rotate 90 degress clockwize
+    """
 
     digits = [
         [0x2, 0x5, 0x5, 0x5, 0x2],
@@ -20,17 +27,19 @@ def compact_number(display, number, offx=0, offy=0):
         [0x2, 0x5, 0x3, 0x1, 0x6],
     ]
     num = []
-    for row in range(0, 5):
+    for row in range(0, len(num[0]):
         num.append(
             "{0:08b}".format(digits[number // 10][row] << 4 | digits[number % 10][row])
         )
+    if rotate > 0:
+        for i in range(0,rotate):
+            num = list(zip(*num[::-1]))
     display.fill(0)
     for y in range(0, len(num)):
-        for x in range(0, 8):
+        for x in range(0, len(num[y])):
             if num[y][x] == "1":
                 display.pixel(x + offx, y + offy, 1)
     display.show()
-
 
 if __name__ == "__main__":
     spi = SPI(0, baudrate=10000000, polarity=1, phase=0, sck=Pin(2), mosi=Pin(3))
@@ -42,5 +51,8 @@ if __name__ == "__main__":
     display.show()
 
     for number in range(0, 100):
-        compact_number(display, number)
-        time.sleep(0.2)
+        compact_number(display, number,rotate=3)
+        time.sleep(0.1)
+    for number in range(0, 100):
+        compact_number(display, 99-number,rotate=3)
+        time.sleep(0.1)
